fca8169b73fa9563e3b91d664de087a12b3a7520
talexcrowell <talexcrowell@gmail.com>
2019-02-14 12:56:36 -0800

    added demo account, updated readme, css tweaks

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Noteful
-[Client](https://noted-client.herokuapp.com)
-[Server](https://noted-server.herokuapp.com/api)
+- [Client](https://noted-client.herokuapp.com)
+- [Server](https://noted-server.herokuapp.com/api)
+![Screenshot](https://i.imgur.com/ro6IbMb.png)
+![Screenshot 2](https://i.imgur.com/0rtvoY8.png)
+![Screenshot 3](https://i.imgur.com/m7dmF86.png)
+
+## Summary
+Noted is an application focused on helping users learn musical notation and improve their skills as a classical musician. Utilizing a spaced repetition algorithm, a user's question queue is built based on if concepts are answered correctly with empahsis on incorrect questions appearing more often. Users can track their learning progress by comparing between sessions.
 
 ## Tech Stack
 - React
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
+import {login} from '../actions/auth';
 
 
 import  RegistrationPage  from './registration-page';
 
-export function LandingPage(props) {
+export class LandingPage extends React.Component {
     // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+    render(){
+        
+        // If we are logged in redirect straight to the user's dashboard
+        if (this.props.loggedIn) {
+            return <Redirect to="/dashboard" />;
+        }
 
-    return (
-        <div className="home">
-            <h2>Welcome to Noted</h2>
-            <p>Learn a plethora of music notation symbols and how understanding them can improve your skills as a musician!</p>
-            <RegistrationPage />
-        </div>
-    );
+        return (
+            <div className="home">
+                <h2>Welcome to Noted</h2>
+                <p>Learn a plethora of music notation symbols and how understanding them can improve your skills as a musician! Try it out!</p>
+                <button className='demo' onClick={() => this.props.dispatch(login('demouser', 'password123'))}>Demo</button>
+                <p className='divider'>... or make an account!</p>
+                <RegistrationPage />
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
